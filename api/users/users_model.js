515b155d7ab214140d9b2b6efd6752bb0cd08230
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-20 11:13:42 -0600

    setting up user-model

--- api/users/users_model.js
+++ api/users/users_model.js
@@ -1,1 +1,15 @@
 const db = require('../../data/dbConfig');
+
+function findById(id) {
+    return db('users').select('username').where('id', id).first()
+}
+
+async function add ({ username, password }) {
+    const [id] = await db('users').insert
+    return findById(id)
+}
+
+module.exports = {
+    add,
+    findById,
+}
