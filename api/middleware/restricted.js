c7e59f9aee4b02aa858abef08fbe39913a4062dd
andrewsbusby <64098862+andrewsbusby@users.noreply.github.com>
2021-08-20 10:25:35 -0600

    setting up restricted middleware

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -2,7 +2,19 @@ const { JWT_SECTECT } = require('../secret/secrec');
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-  next();
+  
+  const token = req.headers.authorization
+    if(!token) {
+      return next({ status: 401, message: 'Token required'})
+    } 
+    jwt.verity(token, JWT_SECTECT, (err, decodedToken) => {
+      if (err) {
+        next({ status: 401, message: 'Token invalid' })
+      } else {
+        req.decodedToken = decodedToken
+        next()
+      }
+    })
   /*
     IMPLEMENT
 
